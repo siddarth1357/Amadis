c79b60ad7c60b4eea208e87642fe1274ec907084
rksid <rksid@example.com>
2026-07-22 09:03:25 +0530

    Add Day 5 task 1 and task 2

---
+++ Day-5/task-1/script.js
@@ -0,0 +1,9 @@
+let fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grape'];
+let removed = fruits.splice(1, 2);
+console.log(removed);        
+console.log(fruits);         
+fruits.splice(-1, 1);
+console.log(fruits);        
+fruits.splice(1);
+console.log(fruits);         
+                           
